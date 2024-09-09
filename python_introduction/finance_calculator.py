38d9737870bbaa75aa3b6045ad69f3873669c229
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-09-09 14:07:35 +0100

    finance cal

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
-total_monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+total_monthly_income = float(input("Enter your monthly income: "))
+total_monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_saving = int(total_monthly_income - total_monthly_expenses)
+monthly_saving = float(total_monthly_income - total_monthly_expenses)
 
 savings = monthly_saving * 12 + (monthly_saving * 12 * 0.05)
 
