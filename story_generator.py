76292da0094da2b19cef3366139b54a9e0da2e96
Alexander Sibhatu <sibhatualexander@gmail.com>
2024-09-16 22:13:18 +0100

    removed

--- story_generator.py
+++
@@ -1,7 +0,0 @@
-user_day = input('Enter a noun of the special day:')
-
-if user_day:
-    print(f"On a beautiful {user_day} day, I went to the zoo."
-          f"I saw a funny {user_day} monkey swinging from the trees. "
-          f"Then, I spotted a majestic {user_day} lion lounging in the sun. "
-          f"What a wild and {user_day} experience!")
